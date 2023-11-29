9a871af938085415e184529ed238305117ed34b1
Michael van de Water <me@mjvdw.com>
2023-11-30 12:14:18 +1300

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import importlib
+import click
+from aocd import get_data, submit
+from rich.console import Console
+
+@click.command
+@click.argument("puzzle_code")
+@click.option('--send', default=False, help="Whether to submit the answer to AOCD")
+def run(puzzle_code: str, send):
+    year = 2023
+    day = int(puzzle_code[0:-1])
+    part = puzzle_code[-1]
+
+    c = Console()
+    data = get_data(year=year, day=day)
+    c.rule(f"Advent of Code {year}-{day}{part}")
+    ####################################################################################
+
+    parsed = [*data]    
+    
+    working = importlib.import_module(f"working.day_{day:03d}{part}")
+    answer = working.go(parsed)
+
+    ####################################################################################
+    c.rule("Finished")
+
+    if send:
+        submit(year=year, day=day, part=part, answer=answer)
+
+
+if __name__ == "__main__":
+    run()
---
+++ working/day_001a.py
@@ -0,0 +1,2 @@
+def go(data):
+    print("Hello!")
