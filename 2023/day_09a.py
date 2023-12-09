555b899bba9a5bd48bb9380717d2a0c367d54ee7
Michael van de Water <me@mjvdw.com>
2023-12-09 19:51:41 +1300

    Day 9

---
+++ 2023/day_09a.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+sample_data = """0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"""
+
+sample_data_answer = 114
+
+
+def go(data):
+    # data = sample_data
+
+    histories = [[int(i) for i in h.split()] for h in data.splitlines()]
+
+    answer = 0
+
+    for history in histories:
+        differences = [history]
+        while len(set(differences[-1])) > 1:
+            differences.append([x for x in np.diff(differences[-1])])
+
+        i = 0
+        next_value = differences[-1][-1]
+        while i < len(differences) - 1:
+            next_value += differences[-i - 2][-1]
+            i += 1
+
+        answer += next_value
+
+    print(answer, f"({'Too low' if answer <= 1688332961 else 'Might be right?'})")
+    return answer
