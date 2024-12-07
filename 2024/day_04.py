b464b41b24a4f99b0afa58dbc44ae57c2a79c5dc
Michael van de Water <36352239+mjvdw@users.noreply.github.com>
2024-12-07 13:41:52 +1300

    Day 4

--- 2024/day_04.py
+++ 2024/day_04.py
@@ -1,5 +1,6 @@
 from martyns_brain import Vector, DIAGONAL_NEIGHBOURS
 from collections import defaultdict
+from aocd import get_data
 
 sample_data = """\
 MMMSXXMASM
@@ -23,26 +24,54 @@ def go(data):
             values[Vector(i, j)] = char
 
     part1 = 0
+    xs = dict(filter(lambda x: x[1] == "X", values.items()))
+    for x in xs:
+        part1 += find_xmas(x, DIAGONAL_NEIGHBOURS, values)
 
-    for x in dict(filter(lambda x: x[1] == "x", values.items())):
-        print("Something")
-        part1 += find_xmas(x, DIAGONAL_NEIGHBOURS, values, "MAS")
+    part2 = 0
+    a_s = dict(filter(lambda x: x[1] == "A", values.items()))
+    for a in a_s:
+        part2 += find_meta_xmas(
+            a,
+            [Vector(1, 1), Vector(-1, 1), Vector(1, -1), Vector(-1, -1)],
+            values,
+        )
 
-    print(part1)
-
-    return part1
+    return part1, part2
 
 
-def find_xmas(vector, directions, values, xmas):
-    for i in range(1, len(xmas)):
-        valid_directions = []
-        for d in directions:
-            if (values[vector + (d * i)]) == xmas[i]:
-                valid_directions.append(d)
+def find_meta_xmas(vector, directions, values):
+    letters = []
+    letters = "".join([values[vector + d] for d in directions])
 
-    return 1
+    # This doesn't work and I don't understand why.
+    # It seems to be picking up scenarios that shouldn't count.
+    # Works for sample data, but not for real data.
+
+    # if "".join(sorted(letters)) == "MMSS":
+    #     return 1
+
+    # I do understand why this works, but it seems like it should be the same.
+    if letters in {"MMSS", "SMSM", "SSMM", "MSMS"}:
+        return 1
+
+    return 0
+
+
+def find_xmas(vector, directions, values, xmas="MAS"):
+    words = 0
+    for d in directions:
+        count = 0
+        for i in range(1, len(xmas) + 1):
+            if (values[vector + (d * i)]) == xmas[i - 1]:
+                count += 1
+        if count == len(xmas):
+            words += 1
+
+    return words
 
 
 if __name__ == "__main__":
-    assert go(sample_data) == 18
-    # print(go(data))
+    data = get_data(day=4, year=2024)
+    print(go(sample_data))
+    print(go(data))
